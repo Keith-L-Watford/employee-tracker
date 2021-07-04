ff9e0e32cadfcba65949457113952920f58e391b
Keith Watford <keith.l.watford@gmail.com>
2021-07-04 13:25:39 -0400

    set up the schema

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -4,8 +4,24 @@ CREATE DATABASE employeeDB;
 
 USE employeeDB;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT
+    name VARCHAR(30) NOT NULL --department name
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT
+    title VARCHAR(30) NOT NULL--role title
+    salary DECIMAL(6, 0) NOT NULL--hold role's salary
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT
-
+    first_name VARCHAR(30) NOT NULL --employee's first name
+    last_name VARCHAR(30) NOT NULL --employee's last name
+    role_id INT NOT NULL -- employee's assigned ID number
+    manager_id INT NOT NULL --the employee's Mananger's assigned ID number
     PRIMARY KEY (id)
 );
