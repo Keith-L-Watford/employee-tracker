39d6d227bfa50b0cf9a6202cb122b960b2aa7f2d
Keith Watford <keith.l.watford@gmail.com>
2021-07-06 16:23:08 -0400

    fixed puncuation

--- employeeDB.sql
+++ employeeDB.sql
@@ -6,29 +6,29 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT
-    name VARCHAR(30) NOT NULL --department name
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL, -- department name
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT
-    title VARCHAR(30) NOT NULL--role title
-    salary DECIMAL(6, 0) NOT NULL--hold role's salary
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,-- role title
+    salary DECIMAL(6, 0) NOT NULL,-- hold role's salary
     PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT
-    first_name VARCHAR(30) NOT NULL --employee's first name
-    last_name VARCHAR(30) NOT NULL --employee's last name
-    role_id INT NOT NULL -- employee's assigned ID number
-    manager_id INT NOT NULL --the employee's Mananger's assigned ID number
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL, -- employee's first name
+    last_name VARCHAR(30) NOT NULL, -- employee's last name
+    role_id INT NOT NULL, -- employee's assigned ID number
+    manager_id INT NOT NULL, -- the employee's Mananger's assigned ID number
     PRIMARY KEY (id)
 );
 
 
---Hardcoding some seed data for the database
+-- Hardcoding some seed data for the database
 INSERT INTO department (name)
 VALUES ("Education");
 
