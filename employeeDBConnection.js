2455debc744ad938b4416ae767deda9beb8c799a
Keith Watford <keith.l.watford@gmail.com>
2021-07-06 17:27:43 -0400

    refactored the if

--- employeeDBConnection.js
+++ employeeDBConnection.js
@@ -1,35 +1,63 @@
 require('dotenv').config()
-const mysql      = require('mysql');
+const mysql = require('mysql');
 const inquirer = require("inquirer");
-
+// const cTable = require('console.table');
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME,
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 
-inquirer.prompt([
-    {
-        
-    },
-])
-    .then((data) => {
+function startApp() {
+  inquirer
+    .prompt([{
+      // for Create, Read, Update
+      name: 'cru',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View Existing', 'Create New', 'Update Current', "EXIT"],
+    }, ]).then((data) => {
 
-});
+      switch (data.cru) {
+        case 'View Existing':
+          readData();
+          break;
+        case 'Create New':
+          writeData();
+          break;
+        case 'Update Current':
+          updateData();
+          break;
+        default:
+          connection.end();
+          break;
+      }
+    });
+};
 
 
+const readData = () => {
+  console.log('this is read test');
+  connection.end();
+}
 
+const writeData = () => {
+  console.log('this is write test');
+  connection.end();
+}
 
-
-
+const updateData = () => {
+  console.log('this is update test');
+  connection.end();
+}
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    connection.end();
-  });
-  
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  // connection.end();
+  startApp();
+});
